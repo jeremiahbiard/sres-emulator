2337dda2ee192a7abf9cc327927fefedad196992
Jeremiah Biard <jeremiahbiard@gmail.com>
2018-06-03 17:45:56 -0400

    stub out header info utils

--- src/rom_utils.rs
+++ src/rom_utils.rs
@@ -6,6 +6,27 @@ use std::path::Path;
 use std::error::Error;
 use std::result::Result;
 
+#[derive(Debug)]
+enum RomType {
+    LoRom,
+    HiRom,
+}
+
+#[derive(Debug)]
+struct RomHeader {
+    rom_name: String,
+    rom_type: RomType,
+}
+
+impl RomHeader {
+    fn new(rom_name: String, rom_type: RomType) -> RomHeader {
+        RomHeader {
+            rom_name,
+            rom_type,
+        }
+    }
+}
+
 pub fn load_rom(path: &Path) -> Result<Vec<u8>, Box<Error>> {
 
     // TODO: Add better error handling here.
@@ -17,8 +38,16 @@ pub fn load_rom(path: &Path) -> Result<Vec<u8>, Box<Error>> {
         let mut archive = zip::ZipArchive::new(&mut file)?;
         let mut rom = archive.by_index(0)?;
         rom.read_to_end(&mut program)?;
-        return Ok(program);
-    }
-    file.read_to_end(&mut program)?;
+    } else {
+        file.read_to_end(&mut program)?;
+    };
+
+    let h: RomHeader = extract_header_info(&program);
+    println!("{:?}", h); 
     Ok(program)
+}
+
+// TODO: actually get the info from the rom
+fn extract_header_info(_program: &Vec<u8>) -> RomHeader {
+   RomHeader::new("SUPER MARIOWORLD".to_string(), RomType::LoRom)
 }

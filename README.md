c48926716600afcb5e19951fde4930abb921c6a9
Jeremiah Biard <jeremiahbiard@gmail.com>
2018-06-03 16:33:51 -0400

    add travis stuff

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# sres-emulator
-Super Rustendo Emulator System
+[![Build Status](https://travis-ci.org/jeremiahbiard/sres-emulator.svg?branch=master)](https://travis-ci.org/jeremiahbiard/sres-emulator)
+
+# Super Rustendo Emulator System
+
+Super Nintendo Entertainment System emulator written in Rust
--- src/rom_utils.rs
+++ src/rom_utils.rs
@@ -5,10 +5,11 @@ use std::io::Read;
 use std::path::Path;
 use std::error::Error;
 use std::result::Result;
-use std::ffi::OsStr;
+use self::zip::read;
 
 pub fn load_rom(path: &Path) -> Result<Vec<u8>, Box<Error>> {
 
+    // TODO: Add better error handling here.
     let extension = path.extension().unwrap();
     if extension.to_str() == Some(&"zip") {
         return load_zip(path)
@@ -21,10 +22,11 @@ pub fn load_rom(path: &Path) -> Result<Vec<u8>, Box<Error>> {
 }
 
 fn load_zip(path: &Path) -> Result<Vec<u8>, Box<Error>> {
-    println!("ZIP");
+    /*
     let mut fh = fs::File::open(&path)?;
     let mut program = Vec::new();
 
     fh.read_to_end(&mut program)?;
     Ok(program)
+    */
 }

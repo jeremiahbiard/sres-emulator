ae28fe8e8754983a5a93a6359f62b6f1aaa1ccd2
Jeremiah Biard <jeremiahbiard@gmail.com>
2018-06-03 17:18:58 -0400

    refactor to remove load_zip and move functionality to load_rom because DRY

--- src/rom_utils.rs
+++ src/rom_utils.rs
@@ -9,24 +9,16 @@ use std::result::Result;
 pub fn load_rom(path: &Path) -> Result<Vec<u8>, Box<Error>> {
 
     // TODO: Add better error handling here.
+    let mut file = fs::File::open(&path)?; 
     let extension = path.extension().unwrap();
-    if extension.to_str() == Some(&"zip") {
-        return load_zip(path)
-    }
-    let mut fh = fs::File::open(&path)?;
     let mut program = Vec::new(); 
     
-    fh.read_to_end(&mut program)?;
-    Ok(program)
-}
-
-fn load_zip(path: &Path) -> Result<Vec<u8>, Box<Error>> {
-
-    let mut file = fs::File::open(&path)?;
-    let mut archive = zip::ZipArchive::new(&mut file)?;
-    let mut rom = archive.by_index(0)?;
-    let mut program = Vec::new();
-    rom.read_to_end(&mut program)?;
-
+    if extension.to_str() == Some(&"zip") {
+        let mut archive = zip::ZipArchive::new(&mut file)?;
+        let mut rom = archive.by_index(0)?;
+        rom.read_to_end(&mut program)?;
+        return Ok(program);
+    }
+    file.read_to_end(&mut program)?;
     Ok(program)
 }
